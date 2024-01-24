0e9ac2b3cb853f87732ba13f7f3f63a297b0953b
Sandra Fan <78048660+sbf99@users.noreply.github.com>
2024-01-23 23:07:16 -0800

    Update run.sh
    Update script to pick any file that begins with case-insensitive "sanju" and ends in "mp4" in any USB drive.

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-video_path="`find /media/sbf/ -name 'all_videos.*'`"
-##video_path="`find /media/sbf/ -name 'AllVideosTest.*'`"
+video_path="`find /media/sbf/ -iname 'sanju*mp4'`"
 
 [[ -z "$video_path" ]] && { echo "No video found. Exiting."; exit 1; }
 
-python ~/SanjuTree/playVideo.py -fs --video=$video_path
+python ~/SanjuTree/playVideo.py -fs --video="$video_path"
